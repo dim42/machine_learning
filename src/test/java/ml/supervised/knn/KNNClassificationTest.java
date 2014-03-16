d79b4636acddf427dc767b779ee1ad2250a2477b
dim42 <dimt777@gmail.com>
2014-03-16 22:20:24 +0400

    +junit4.11 tests

---
+++ src/test/java/ml/supervised/knn/KNNClassificationTest.java
@@ -0,0 +1,17 @@
+package ml.supervised.knn;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+public class KNNClassificationTest {
+
+    @Test
+    public void testClassifyTest() throws IOException {
+        KNNClassification classification = new KNNClassification();
+        int errorCount = classification.classifyTest();
+        assertEquals(36, errorCount);
+    }
+}
---
+++ src/test/java/ml/unsupervised/clustering/KMeansClusteringTest.java
@@ -0,0 +1,22 @@
+package ml.unsupervised.clustering;
+
+import static ml.unsupervised.clustering.KMeansClustering.CLUSTER_NUMBER;
+import static ml.unsupervised.clustering.KMeansClustering.INPUT_FILE_NAME;
+
+import java.io.IOException;
+
+import org.junit.Test;
+
+import Jama.Matrix;
+
+public class KMeansClusteringTest {
+
+    @Test
+    public void testKMeans() throws IOException {
+        KMeansClustering clustering = new KMeansClustering();
+        Matrix dataSet = clustering.loadDataSet(INPUT_FILE_NAME);
+
+        KMeansResult result = clustering.kMeans(dataSet, CLUSTER_NUMBER);
+        System.out.println(result);
+    }
+}
